1b2e686ed68d9526995fbe2301ebac986b7f6f76
Kodie Grantham <kodie.grantham@gmail.com>
2024-12-17 15:53:31 -0600

    Update plugin hook descriptions

--- inc/plugins.php
+++ inc/plugins.php
@@ -1,10 +1,10 @@
 <?php
-// Gravity Forms: Disables the theme (so we can create a custom one)
+// Gravity Forms: Disable the built-in theme (so we can create a custom one)
 add_filter('gform_disable_form_theme_css', '__return_true');
 add_filter('gform_disable_form_legacy_css', '__return_true');
 
 
-// Updraft Plus: Prevents backups being created for non-production environments
+// Updraft Plus: Prevent backups being created for non-production environments
 add_filter('updraftplus_boot_backup', 'primer_prevent_non_prod_backups');
 function primer_prevent_non_prod_backups($do_backup) {
   if (wp_get_environment_type() !== 'production') return false;
@@ -12,7 +12,7 @@ function primer_prevent_non_prod_backups($do_backup) {
 }
 
 
-// Yoast SEO: Custom breadcrumb separator when using the yoast_breadcrumb function
+// Yoast SEO: Custom breadcrumb separator when using the `yoast_breadcrumb` function
 add_filter('wpseo_breadcrumb_separator', 'primer_breadcrumb_separator');
 function primer_breadcrumb_separator($separator) {
   return ' / ';
