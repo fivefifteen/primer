1cccc14549ae3dc6e595c9f47bc21f9d6e98c8cf
Kodie Grantham <kodie.grantham@gmail.com>
2024-12-19 15:37:54 -0600

    Remove title tag from header to allow WordPress to handle it

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!doctype html>
   <head>
     <meta charset="utf-8">
-    <title><?php wp_title(''); ?></title>
     <meta name="viewport" content="width=device-width">
     <?php wp_head(); ?>
   </head>
