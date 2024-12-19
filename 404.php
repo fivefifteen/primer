7617dc855270ecaa3330e5052905606a474b6154
Kodie Grantham <kodie.grantham@gmail.com>
2024-12-19 11:05:59 -0600

    Add 404 template

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section>
+  <div class="container">
+    <h1 class="page-title">Page not found</h1>
+    <p>It looks like nothing was found at this location. Head back to the <a href="<?=home_url()?>">home page</a>?</p>
+  </div>
+</section>
+
+<?php get_footer(); ?>
